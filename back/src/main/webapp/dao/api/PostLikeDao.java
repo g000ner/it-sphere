a2539fc85f195dea50ff3061d5497808b74cb796
Dmirty Kiselev <kiselevzfth@gmail.com>
2023-03-10 21:12:32 +0400

    написал PostLikeDao, PostLikeDaoException

--- back/src/main/webapp/dao/api/PostLikeDao.java
+++ back/src/main/webapp/dao/api/PostLikeDao.java
@@ -1,4 +1,14 @@
 package main.webapp.dao.api;
 
+import main.webapp.domain.post.Post;
+import main.webapp.domain.post.PostComment;
+import main.webapp.domain.post.PostLike;
+import main.webapp.domain.user.User;
+import main.webapp.exception.dao.PostLikeDaoException;
+
+import java.util.List;
+
 public interface PostLikeDao {
+    void addLike(PostLike like) throws PostLikeDaoException;
+    List<PostLike> getCommentsByPost(Post post) throws PostLikeDaoException;
 }
